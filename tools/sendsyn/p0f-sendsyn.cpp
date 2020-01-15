9432ae4e262d249797b590a40aec734cfedceaee
Evan Teran <evan.teran@gmail.com>
2020-01-15 00:51:58 -0500

    formatting

--- tools/sendsyn/p0f-sendsyn.cpp
+++ tools/sendsyn/p0f-sendsyn.cpp
@@ -14,170 +14,162 @@
 
  */
 
+#include <cerrno>
 #include <cstdio>
 #include <cstdlib>
-#include <unistd.h>
 #include <cstring>
 #include <netdb.h>
-#include <cerrno>
+#include <unistd.h>
 
-#include <netinet/in.h>
 #include <arpa/inet.h>
-#include <sys/types.h>
-#include <sys/time.h>
+#include <netinet/in.h>
 #include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/types.h>
 
 #include "config.h"
 #include "debug.h"
 #include "tcp.h"
 
-
 /* Do a basic IPv4 TCP checksum. */
 
-static void tcp_cksum(uint8_t* src, uint8_t* dst, struct tcp_hdr* t, uint8_t opt_len) {
+static void tcp_cksum(uint8_t *src, uint8_t *dst, struct tcp_hdr *t, uint8_t opt_len) {
 
-  uint32_t sum, i;
-  uint8_t* p;
+	uint32_t sum, i;
+	uint8_t *p;
 
-  if (opt_len % 4) FATAL("Packet size not aligned to 4.");
+	if (opt_len % 4) FATAL("Packet size not aligned to 4.");
 
-  t->cksum = 0;
+	t->cksum = 0;
 
-  sum = PROTO_TCP + sizeof(struct tcp_hdr) + opt_len;
+	sum = PROTO_TCP + sizeof(struct tcp_hdr) + opt_len;
 
-  p = (uint8_t*)t;
+	p = (uint8_t *)t;
 
-  for (i = 0; i < sizeof(struct tcp_hdr) + opt_len; i += 2, p += 2)
-    sum += (*p << 8) + p[1];
+	for (i = 0; i < sizeof(struct tcp_hdr) + opt_len; i += 2, p += 2)
+		sum += (*p << 8) + p[1];
 
-  p = src;
+	p = src;
 
-  for (i = 0; i < 4; i += 2, p += 2) sum += (*p << 8) + p[1];
+	for (i = 0; i < 4; i += 2, p += 2)
+		sum += (*p << 8) + p[1];
 
-  p = dst;
+	p = dst;
 
-  for (i = 0; i < 4; i += 2, p += 2) sum += (*p << 8) + p[1];
+	for (i = 0; i < 4; i += 2, p += 2)
+		sum += (*p << 8) + p[1];
 
-  t->cksum = htons(~(sum + (sum >> 16)));
-
+	t->cksum = htons(~(sum + (sum >> 16)));
 }
-
 
 /* Parse IPv4 address into a buffer. */
 
-static void parse_addr(char* str, uint8_t* ret) {
+static void parse_addr(char *str, uint8_t *ret) {
 
-  uint32_t a1, a2, a3, a4;
+	uint32_t a1, a2, a3, a4;
 
-  if (sscanf(str, "%u.%u.%u.%u", &a1, &a2, &a3, &a4) != 4)
-    FATAL("Malformed IPv4 address.");
+	if (sscanf(str, "%u.%u.%u.%u", &a1, &a2, &a3, &a4) != 4)
+		FATAL("Malformed IPv4 address.");
 
-  if (a1 > 255 || a2 > 255 || a3 > 255 || a4 > 255)
-    FATAL("Malformed IPv4 address.");
+	if (a1 > 255 || a2 > 255 || a3 > 255 || a4 > 255)
+		FATAL("Malformed IPv4 address.");
 
-  ret[0] = a1;
-  ret[1] = a2;
-  ret[2] = a3;
-  ret[3] = a4;
+	ret[0] = a1;
+	ret[1] = a2;
+	ret[2] = a3;
+	ret[3] = a4;
+}
 
-} 
+#define W(_x) (_x) >> 8, (_x)&0xff
+#define D(_x) (_x) >> 24, ((_x) >> 16) & 0xff, ((_x) >> 8) & 0xff, (_x)&0xff
 
-
-#define W(_x) (_x) >> 8, (_x) & 0xff
-#define D(_x) (_x) >> 24, ((_x) >> 16) & 0xff, ((_x) >> 8) & 0xff, (_x) & 0xff
-
-#define EOL       TCPOPT_EOL
-#define NOP       TCPOPT_NOP
-#define MSS(_x)   TCPOPT_MAXSEG, 4, W(_x)
-#define WS(_x)    TCPOPT_WSCALE, 3, (_x)
-#define SOK       TCPOPT_SACKOK, 2
-#define TS(_x,_y) TCPOPT_TSTAMP, 10, D(_x), D(_y)
+#define EOL TCPOPT_EOL
+#define NOP TCPOPT_NOP
+#define MSS(_x) TCPOPT_MAXSEG, 4, W(_x)
+#define WS(_x) TCPOPT_WSCALE, 3, (_x)
+#define SOK TCPOPT_SACKOK, 2
+#define TS(_x, _y) TCPOPT_TSTAMP, 10, D(_x), D(_y)
 
 /* There are virtually no OSes that do not send MSS. Support for RFC 1323
    and 2018 is not given, so we have to test various combinations here. */
 
 static uint8_t opt_combos[8][24] = {
 
-  { MSS(SPECIAL_MSS), NOP, EOL },                           /* 6  */
+	{MSS(SPECIAL_MSS), NOP, EOL}, /* 6  */
 
-  { MSS(SPECIAL_MSS), SOK, NOP, EOL },                      /* 8  */
+	{MSS(SPECIAL_MSS), SOK, NOP, EOL}, /* 8  */
 
-  { MSS(SPECIAL_MSS), WS(5), NOP, EOL },                    /* 9  */
+	{MSS(SPECIAL_MSS), WS(5), NOP, EOL}, /* 9  */
 
-  { MSS(SPECIAL_MSS), WS(5), SOK, NOP, EOL },               /* 12 */
+	{MSS(SPECIAL_MSS), WS(5), SOK, NOP, EOL}, /* 12 */
 
-  { MSS(SPECIAL_MSS), TS(1337, 0), NOP, EOL },              /* 17 */
+	{MSS(SPECIAL_MSS), TS(1337, 0), NOP, EOL}, /* 17 */
 
-  { MSS(SPECIAL_MSS), SOK, TS(1337, 0), NOP, EOL },         /* 19 */
+	{MSS(SPECIAL_MSS), SOK, TS(1337, 0), NOP, EOL}, /* 19 */
 
-  { MSS(SPECIAL_MSS), WS(5), TS(1337, 0), NOP, EOL },       /* 20 */
+	{MSS(SPECIAL_MSS), WS(5), TS(1337, 0), NOP, EOL}, /* 20 */
 
-  { MSS(SPECIAL_MSS), WS(5), SOK,  TS(1337, 0), NOP, EOL }  /* 22 */
+	{MSS(SPECIAL_MSS), WS(5), SOK, TS(1337, 0), NOP, EOL} /* 22 */
 
 };
 
+int main(int argc, char **argv) {
 
-int main(int argc, char** argv) {
+	static struct sockaddr_in sin;
+	char one = 1;
+	int32_t sock;
+	uint32_t i;
 
-  static struct sockaddr_in sin;
-  char one = 1;
-  int32_t  sock;
-  uint32_t  i;
+	static uint8_t work_buf[MIN_TCP4 + 24];
 
-  static uint8_t work_buf[MIN_TCP4 + 24];
+	struct ipv4_hdr *ip4 = (struct ipv4_hdr *)work_buf;
+	struct tcp_hdr *tcp  = (struct tcp_hdr *)(ip4 + 1);
+	uint8_t *opts        = work_buf + MIN_TCP4;
 
-  struct ipv4_hdr* ip4 = (struct ipv4_hdr*)work_buf;
-  struct tcp_hdr*  tcp = (struct tcp_hdr*)(ip4 + 1);
-  uint8_t *opts = work_buf + MIN_TCP4;
+	if (argc != 4) {
+		ERRORF("Usage: p0f-sendsyn your_ip dst_ip port\n");
+		exit(1);
+	}
 
+	parse_addr(argv[1], ip4->src);
+	parse_addr(argv[2], ip4->dst);
 
-  if (argc != 4) {
-    ERRORF("Usage: p0f-sendsyn your_ip dst_ip port\n");
-    exit(1);
-  }
+	sock = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
 
-  parse_addr(argv[1], ip4->src);
-  parse_addr(argv[2], ip4->dst);
+	if (sock < 0) PFATAL("Can't open raw socket (you need to be root).");
 
-  sock = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
+	if (setsockopt(sock, IPPROTO_IP, IP_HDRINCL, (char *)&one, sizeof(char)))
+		PFATAL("setsockopt() on raw socket failed.");
 
-  if (sock < 0) PFATAL("Can't open raw socket (you need to be root).");
+	sin.sin_family = PF_INET;
 
-  if (setsockopt(sock, IPPROTO_IP, IP_HDRINCL, (char*)&one, sizeof(char)))
-    PFATAL("setsockopt() on raw socket failed.");
+	memcpy(&sin.sin_addr.s_addr, ip4->dst, 4);
 
-  sin.sin_family = PF_INET;
-  
-  memcpy(&sin.sin_addr.s_addr, ip4->dst, 4);
+	ip4->ver_hlen = 0x45;
+	ip4->tot_len  = htons(MIN_TCP4 + 24);
+	ip4->ttl      = 192;
+	ip4->proto    = PROTO_TCP;
 
-  ip4->ver_hlen  = 0x45;
-  ip4->tot_len   = htons(MIN_TCP4 + 24);
-  ip4->ttl       = 192;
-  ip4->proto     = PROTO_TCP;
+	tcp->dport     = htons(atoi(argv[3]));
+	tcp->seq       = htonl(0x12345678);
+	tcp->doff_rsvd = ((sizeof(struct tcp_hdr) + 24) / 4) << 4;
+	tcp->flags     = TCP_SYN;
+	tcp->win       = htons(SPECIAL_WIN);
 
-  tcp->dport     = htons(atoi(argv[3]));
-  tcp->seq       = htonl(0x12345678);
-  tcp->doff_rsvd = ((sizeof(struct tcp_hdr) + 24) / 4) << 4;
-  tcp->flags     = TCP_SYN;
-  tcp->win       = htons(SPECIAL_WIN);
+	for (i = 0; i < 8; i++) {
 
-  for (i = 0; i < 8; i++) {
+		tcp->sport = htons(65535 - i);
 
-    tcp->sport = htons(65535 - i);
+		memcpy(opts, opt_combos[i], 24);
+		tcp_cksum(ip4->src, ip4->dst, tcp, 24);
 
-    memcpy(opts, opt_combos[i], 24);
-    tcp_cksum(ip4->src, ip4->dst, tcp, 24);
+		if (sendto(sock, work_buf, sizeof(work_buf), 0, (struct sockaddr *)&sin,
+				   sizeof(struct sockaddr_in)) < 0) PFATAL("sendto() fails.");
 
-    if (sendto(sock, work_buf, sizeof(work_buf), 0, (struct sockaddr*)&sin,
-        sizeof(struct sockaddr_in)) < 0) PFATAL("sendto() fails.");
+		usleep(100000);
+	}
 
-    usleep(100000);
+	SAYF("Eight packets sent! Check p0f output to examine responses, if any.\n");
 
-  }
-
-  SAYF("Eight packets sent! Check p0f output to examine responses, if any.\n");
-
-  return 0;
-
+	return 0;
 }
-
--- tools/sendsyn6/p0f-sendsyn6.cpp
+++ tools/sendsyn6/p0f-sendsyn6.cpp
@@ -18,177 +18,171 @@
 
  */
 
+#include <cctype>
+#include <cerrno>
 #include <cstdio>
 #include <cstdlib>
-#include <unistd.h>
 #include <cstring>
 #include <netdb.h>
-#include <cerrno>
-#include <cctype>
+#include <unistd.h>
 
-#include <netinet/in.h>
 #include <arpa/inet.h>
-#include <sys/types.h>
-#include <sys/time.h>
+#include <netinet/in.h>
 #include <sys/socket.h>
+#include <sys/time.h>
+#include <sys/types.h>
 
 #include "config.h"
 #include "debug.h"
 #include "tcp.h"
 
 /* Do a basic IPv6 TCP checksum. */
-static void tcp_cksum(uint8_t* src, uint8_t* dst, struct tcp_hdr* t, uint8_t opt_len) {
+static void tcp_cksum(uint8_t *src, uint8_t *dst, struct tcp_hdr *t, uint8_t opt_len) {
 
-  uint32_t sum, i;
-  uint8_t* p;
+	uint32_t sum, i;
+	uint8_t *p;
 
-  if (opt_len % 4) FATAL("Packet size not aligned to 4.");
+	if (opt_len % 4) FATAL("Packet size not aligned to 4.");
 
-  t->cksum = 0;
+	t->cksum = 0;
 
-  sum = PROTO_TCP + sizeof(struct tcp_hdr) + opt_len;
+	sum = PROTO_TCP + sizeof(struct tcp_hdr) + opt_len;
 
-  p = (uint8_t*)t;
+	p = (uint8_t *)t;
 
-  for (i = 0; i < sizeof(struct tcp_hdr) + opt_len; i += 2, p += 2)
-    sum += (*p << 8) + p[1];
+	for (i = 0; i < sizeof(struct tcp_hdr) + opt_len; i += 2, p += 2)
+		sum += (*p << 8) + p[1];
 
-  p = src;
+	p = src;
 
-  for (i = 0; i < 16; i += 2, p += 2) sum += (*p << 8) + p[1];
+	for (i = 0; i < 16; i += 2, p += 2)
+		sum += (*p << 8) + p[1];
 
-  p = dst;
+	p = dst;
 
-  for (i = 0; i < 16; i += 2, p += 2) sum += (*p << 8) + p[1];
+	for (i = 0; i < 16; i += 2, p += 2)
+		sum += (*p << 8) + p[1];
 
-  t->cksum = htons(~(sum + (sum >> 16)));
-
+	t->cksum = htons(~(sum + (sum >> 16)));
 }
-
 
 /* Parse IPv6 address into a buffer. */
 
-static void parse_addr(char* str, uint8_t* ret) {
+static void parse_addr(char *str, uint8_t *ret) {
 
-  uint32_t seg = 0;
-  uint32_t val;
-  
-  while (*str) {
+	uint32_t seg = 0;
+	uint32_t val;
 
-    if (seg == 8) FATAL("Malformed IPv6 address (too many segments).");
+	while (*str) {
 
-    if (sscanf((char*)str, "%x", &val) != 1 ||
-        val > 65535) FATAL("Malformed IPv6 address (bad octet value).");
+		if (seg == 8) FATAL("Malformed IPv6 address (too many segments).");
 
-    ret[seg * 2] = val >> 8;
-    ret[seg * 2 + 1] = val;
+		if (sscanf((char *)str, "%x", &val) != 1 ||
+			val > 65535) FATAL("Malformed IPv6 address (bad octet value).");
 
-    seg++;
+		ret[seg * 2]     = val >> 8;
+		ret[seg * 2 + 1] = val;
 
-    while (isxdigit(*str)) str++;
-    if (*str) str++;
+		seg++;
 
-  }
+		while (isxdigit(*str))
+			str++;
+		if (*str) str++;
+	}
 
-  if (seg != 8) FATAL("Malformed IPv6 address (don't abbreviate).");
+	if (seg != 8) FATAL("Malformed IPv6 address (don't abbreviate).");
+}
 
-} 
+#define W(_x) (_x) >> 8, (_x)&0xff
+#define D(_x) (_x) >> 24, ((_x) >> 16) & 0xff, ((_x) >> 8) & 0xff, (_x)&0xff
 
-
-#define W(_x) (_x) >> 8, (_x) & 0xff
-#define D(_x) (_x) >> 24, ((_x) >> 16) & 0xff, ((_x) >> 8) & 0xff, (_x) & 0xff
-
-#define EOL       TCPOPT_EOL
-#define NOP       TCPOPT_NOP
-#define MSS(_x)   TCPOPT_MAXSEG, 4, W(_x)
-#define WS(_x)    TCPOPT_WSCALE, 3, (_x)
-#define SOK       TCPOPT_SACKOK, 2
-#define TS(_x,_y) TCPOPT_TSTAMP, 10, D(_x), D(_y)
+#define EOL TCPOPT_EOL
+#define NOP TCPOPT_NOP
+#define MSS(_x) TCPOPT_MAXSEG, 4, W(_x)
+#define WS(_x) TCPOPT_WSCALE, 3, (_x)
+#define SOK TCPOPT_SACKOK, 2
+#define TS(_x, _y) TCPOPT_TSTAMP, 10, D(_x), D(_y)
 
 /* There are virtually no OSes that do not send MSS. Support for RFC 1323
    and 2018 is not given, so we have to test various combinations here. */
 
 static uint8_t opt_combos[8][24] = {
 
-  { MSS(SPECIAL_MSS), NOP, EOL },                           /* 6  */
+	{MSS(SPECIAL_MSS), NOP, EOL}, /* 6  */
 
-  { MSS(SPECIAL_MSS), SOK, NOP, EOL },                      /* 8  */
+	{MSS(SPECIAL_MSS), SOK, NOP, EOL}, /* 8  */
 
-  { MSS(SPECIAL_MSS), WS(5), NOP, EOL },                    /* 9  */
+	{MSS(SPECIAL_MSS), WS(5), NOP, EOL}, /* 9  */
 
-  { MSS(SPECIAL_MSS), WS(5), SOK, NOP, EOL },               /* 12 */
+	{MSS(SPECIAL_MSS), WS(5), SOK, NOP, EOL}, /* 12 */
 
-  { MSS(SPECIAL_MSS), TS(1337, 0), NOP, EOL },              /* 17 */
+	{MSS(SPECIAL_MSS), TS(1337, 0), NOP, EOL}, /* 17 */
 
-  { MSS(SPECIAL_MSS), SOK, TS(1337, 0), NOP, EOL },         /* 19 */
+	{MSS(SPECIAL_MSS), SOK, TS(1337, 0), NOP, EOL}, /* 19 */
 
-  { MSS(SPECIAL_MSS), WS(5), TS(1337, 0), NOP, EOL },       /* 20 */
+	{MSS(SPECIAL_MSS), WS(5), TS(1337, 0), NOP, EOL}, /* 20 */
 
-  { MSS(SPECIAL_MSS), WS(5), SOK,  TS(1337, 0), NOP, EOL }  /* 22 */
+	{MSS(SPECIAL_MSS), WS(5), SOK, TS(1337, 0), NOP, EOL} /* 22 */
 
 };
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
 
-  static struct sockaddr_in6 sin;
-  char one = 1;
-  int32_t  sock;
-  uint32_t  i;
+	static struct sockaddr_in6 sin;
+	char one = 1;
+	int32_t sock;
+	uint32_t i;
 
-  static uint8_t work_buf[MIN_TCP6 + 24];
+	static uint8_t work_buf[MIN_TCP6 + 24];
 
-  struct ipv6_hdr* ip6 = (struct ipv6_hdr*)work_buf;
-  struct tcp_hdr*  tcp = (struct tcp_hdr*)(ip6 + 1);
-  uint8_t *opts = work_buf + MIN_TCP6;
+	struct ipv6_hdr *ip6 = (struct ipv6_hdr *)work_buf;
+	struct tcp_hdr *tcp  = (struct tcp_hdr *)(ip6 + 1);
+	uint8_t *opts        = work_buf + MIN_TCP6;
 
+	if (argc != 4) {
+		ERRORF("Usage: p0f-sendsyn your_ip dst_ip port\n");
+		exit(1);
+	}
 
-  if (argc != 4) {
-    ERRORF("Usage: p0f-sendsyn your_ip dst_ip port\n");
-    exit(1);
-  }
+	parse_addr(argv[1], ip6->src);
+	parse_addr(argv[2], ip6->dst);
 
-  parse_addr(argv[1], ip6->src);
-  parse_addr(argv[2], ip6->dst);
+	sock = socket(AF_INET, SOCK_RAW, IPPROTO_IPV6);
 
-  sock = socket(AF_INET, SOCK_RAW, IPPROTO_IPV6);
+	if (sock < 0) PFATAL("Can't open raw socket (you need to be root).");
 
-  if (sock < 0) PFATAL("Can't open raw socket (you need to be root).");
+	if (setsockopt(sock, IPPROTO_IP, IP_HDRINCL, (char *)&one, sizeof(char)))
+		PFATAL("setsockopt() on raw socket failed.");
 
-  if (setsockopt(sock, IPPROTO_IP, IP_HDRINCL, (char*)&one, sizeof(char)))
-    PFATAL("setsockopt() on raw socket failed.");
+	sin.sin6_family = PF_INET6;
 
-  sin.sin6_family = PF_INET6;
-  
-  memcpy(&sin.sin6_addr, ip6->dst, 16);
+	memcpy(&sin.sin6_addr, ip6->dst, 16);
 
-  ip6->ver_tos = ntohl(6 << 24);
-  ip6->pay_len = ntohs(sizeof(struct tcp_hdr) + 24);
-  ip6->proto   = PROTO_TCP;
-  ip6->ttl     = 192;
+	ip6->ver_tos = ntohl(6 << 24);
+	ip6->pay_len = ntohs(sizeof(struct tcp_hdr) + 24);
+	ip6->proto   = PROTO_TCP;
+	ip6->ttl     = 192;
 
-  tcp->dport     = htons(atoi(argv[3]));
-  tcp->seq       = htonl(0x12345678);
-  tcp->doff_rsvd = ((sizeof(struct tcp_hdr) + 24) / 4) << 4;
-  tcp->flags     = TCP_SYN;
-  tcp->win       = htons(SPECIAL_WIN);
+	tcp->dport     = htons(atoi(argv[3]));
+	tcp->seq       = htonl(0x12345678);
+	tcp->doff_rsvd = ((sizeof(struct tcp_hdr) + 24) / 4) << 4;
+	tcp->flags     = TCP_SYN;
+	tcp->win       = htons(SPECIAL_WIN);
 
-  for (i = 0; i < 8; i++) {
+	for (i = 0; i < 8; i++) {
 
-    tcp->sport = htons(65535 - i);
+		tcp->sport = htons(65535 - i);
 
-    memcpy(opts, opt_combos[i], 24);
-    tcp_cksum(ip6->src, ip6->dst, tcp, 24);
+		memcpy(opts, opt_combos[i], 24);
+		tcp_cksum(ip6->src, ip6->dst, tcp, 24);
 
-    if (sendto(sock, work_buf, sizeof(work_buf), 0, (struct sockaddr*)&sin,
-        sizeof(struct sockaddr_in6)) < 0) PFATAL("sendto() fails.");
+		if (sendto(sock, work_buf, sizeof(work_buf), 0, (struct sockaddr *)&sin,
+				   sizeof(struct sockaddr_in6)) < 0) PFATAL("sendto() fails.");
 
-    usleep(100000);
+		usleep(100000);
+	}
 
-  }
+	SAYF("Eight packets sent! Check p0f output to examine responses, if any.\n");
 
-  SAYF("Eight packets sent! Check p0f output to examine responses, if any.\n");
-
-  return 0;
-
+	return 0;
 }
-
